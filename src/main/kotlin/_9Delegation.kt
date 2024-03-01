55a4b52e4d4afeee0a6e2dac2dba306b4d58ca07
Mansoor Nisar <126686934+mansoornisar10p@users.noreply.github.com>
2024-03-01 17:52:19 +0500

    removed commented code

--- src/main/kotlin/_9Delegation.kt
+++ src/main/kotlin/_9Delegation.kt
@@ -3,10 +3,6 @@ interface Printer {
 }
 
 class LaserPrinter(private val toner: Toner) : Printer by toner {
-    // LaserPrinter delegates the 'print' function to the 'toner' object
-    // This means that when 'print' is called on a LaserPrinter instance,
-    // it will delegate the call to the 'print' function of the 'toner' object.
-
     fun printDocument() {
         println("Printing document...")
         print() // This calls the 'print' function of the 'toner' object
